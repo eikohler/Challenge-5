08cf97349eb8f6282a78999b5adc97844dcb04e2
Eric Kohler <erickohler1600@gmail.com>
2022-03-27 22:06:33 -0400

    Added time blocks to be color coded based on current time in the day, can enter a note in the time block and click save icon and it will add the note value to local storage under the index of the timeblock, when page is refreshed the saved notes or filled in based on the index of timeblock they were saved from

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,50 @@
+const startTime = 9;
+var counter = startTime;
+
+const d = new Date();
+let hour = d.getHours();
+
+var timeBlocks = $('.time-block');
+
+var notes = getSavedNotes();
+
 $( document ).ready(function() {
     $("#currentDay").text($.datepicker.formatDate('DD, M d', new Date()));
+    
+    for (let i = 0; i < notes.length; i++) {     
+        var note = JSON.parse(notes[i]);
+        timeBlocks.eq(note.position).val(note.plan);
+    }
+
+    timeBlocks.each(function() {
+        if(counter === hour){
+            $(this).addClass("present");
+        }else if(counter < hour){
+            $(this).addClass("past");
+        }else{
+            $(this).addClass("future");
+        }
+        counter++;
+    });
 });
+
+$('.fa-save').click(function(){
+    var index = $('.fa-save').index(this);
+    var text = timeBlocks.eq(index).val();
+  
+    var note = {
+        plan: text,
+        position: index
+    }
+    window.localStorage.setItem('note'+index, JSON.stringify(note));
+});
+
+function getSavedNotes(){
+    var values = [];
+    var keys = Object.keys(localStorage);
+    var i = keys.length;
+    while ( i-- ) {
+        values.push( localStorage.getItem(keys[i]));
+    }
+    return values;
+}
