7d7d69232936e8966a1663af4ae2c5bbcdec4afd
Eric Kohler <erickohler1600@gmail.com>
2022-03-27 22:22:54 -0400

    Add comments and update readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Work Day Scheduler Starter Code
+Created a work day schedule web app that uses jQuery, Bootstrap, jQuery UI and font awesome
+icons third party API.
+Displays 9-5 working hours in the day and color codes based on time of the day, i.e, past,
+present or future. Displays the current date at the top of the page.
+When then user enters a note in a specific time block and clicks the subsequent save icon
+the note is saved for that timeblock and remains in local storage to be pulled from and
+filled back in if the page is refreshed.
+
+![Screen Shot](screenshot.JPG?raw=true "Screen Shot")
+URL: https://eikohler.github.io/Challenge-5/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-const startTime = 9;
-var counter = startTime;
+const startTime = 9; //Start at 9AM
+var counter = startTime; //Used for incrementing through the hours 9->5
 
-const d = new Date();
-let hour = d.getHours();
+const d = new Date(); //Gets current date
+let hour = d.getHours(); //Gets current hour
 
-var timeBlocks = $('.time-block');
+var timeBlocks = $('.time-block'); //Get timeblock elements
 
-var notes = getSavedNotes();
+var notes = getSavedNotes(); //Gets all saved notes
 
 $( document ).ready(function() {
-    $("#currentDay").text($.datepicker.formatDate('DD, M d', new Date()));
+    //Displays the current day at top
+    $("#currentDay").text($.datepicker.formatDate('DD, M d', new Date())); 
     
+    //Fills in saved note values by index of timeblock class element
     for (let i = 0; i < notes.length; i++) {     
         var note = JSON.parse(notes[i]);
         timeBlocks.eq(note.position).val(note.plan);
     }
 
+    //Adds required css class based on relation to current time of the day
     timeBlocks.each(function() {
         if(counter === hour){
             $(this).addClass("present");
@@ -28,6 +31,8 @@ $( document ).ready(function() {
     });
 });
 
+// When save icon is clicked saves the message to local storage with the index the
+// message came from
 $('.fa-save').click(function(){
     var index = $('.fa-save').index(this);
     var text = timeBlocks.eq(index).val();
@@ -39,6 +44,7 @@ $('.fa-save').click(function(){
     window.localStorage.setItem('note'+index, JSON.stringify(note));
 });
 
+//Returns an array of all the notes in local storage 
 function getSavedNotes(){
     var values = [];
     var keys = Object.keys(localStorage);
